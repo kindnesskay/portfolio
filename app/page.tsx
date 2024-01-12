b625709d5056ddeab3ab5ff1b6f9528f57f7a2eb
kindesskay <kindenessudubamo@gmail.com>
2024-01-12 19:17:36 +0100

    ui update

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,23 @@
+import Lamp from "@/components/lamp";
 import Link from "next/link";
 
 export default function page() {
   return (
-    <section className="flex flex-col w-full h-screen bg-black items-center justify-center">
-      <div>
-        <h1 className="text-center text-white font-bold text-xl md:text-3xl">Kay's</h1>
-        <h2 className="text-center text-white font-bold text-3xl md:text-7xl">PORTFOLIO</h2>
-      </div>
-      <div className="text-white flex gap-4 font-semibold font-mono text-md md:text-lg">
-        <Link href={'https://twitter.com/officialkay_ok'}>Twitter</Link>
-        <Link href={'https://github.com/kindnesskay'}>Github</Link>
+    <section className="flex flex-col w-full h-screen justify-center items-center">
+      <Lamp />
+      <div  className="flex flex-col justify-center z-10">
+        <div>
+          <h1 className="text-center text-white font-bold text-xl sm:text-xl md:text-3xl">
+            Kay's
+          </h1>
+          <h2 className="text-center text-white font-bold text-3xl md:text-7xl sm:text-5xl">
+            PORTFOLIO
+          </h2>
+        </div>
+        <div className="text-white flex gap-4 justify-center font-semibold font-mono text-md md:text-lg">
+          <Link href={"https://twitter.com/officialkay_ok"}>Twitter</Link>
+          <Link href={"https://github.com/kindnesskay"}>Github</Link>
+        </div>
       </div>
     </section>
   );
---
+++ components/lamp.tsx
@@ -0,0 +1,7 @@
+export default function Lamp() {
+  return (
+    <div className="w-full h-full flex flex-col absolute  items-center bg-gradient-to-b from-black to-blue-950">
+      <div className=" w-3/4 h-16 bg-black shadow-2xl shadow-blue-800 rounded-lg z-10"></div>
+    </div>
+  );
+}
